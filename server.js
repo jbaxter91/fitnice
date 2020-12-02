86db962411f7eb6f017afbbaf10cab496e704fc4
John Baxter <jbaxter@a-zdarts.com>
2020-12-02 12:26:22 -0800

    Scratch that, doing everything in server.js

--- server.js
+++ server.js
@@ -1,31 +1,33 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const compression = require("compression");
+const path = require("path")
 
-require("dotenv/config");
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
 
 const app = express();
-const PORT = process.env.PORT || 8080;
 
-//Middleware
 app.use(logger("dev"));
-app.use(express.urlencoded({extended: true}));
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static("public"));
-app.use(compression());
-app.use(require("./routes/html-routes"));
-app.use(require("./routes/api-routes"));
 
-mongoose.connect(process.env.MONGODB_URI,
-     {
-         userNewUrlParser: true,
-         useUnifiedTopology: true,
-         useCreateIndex: true,
-         useFindAndModify: false
-    }, ()=> {
-    console.log(`Connected to db`);
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/workout", 
+  { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 
-app.listen(PORT, ()=> console.log(`Server started on PORT: ${PORT}`));
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
